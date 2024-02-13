9de9f154117860a52cb02c19e1ec40f94bd1da6a
victor.x.qu <fs7744@hotmail.com>
2024-02-13 10:46:44 +0800

    init

--- src/RuQu/IInput.cs
+++ src/RuQu/IInput.cs
@@ -1,5 +1,12 @@
 ﻿namespace RuQu
 {
+    public interface IPeeker<T>
+    { 
+        int Readed { get; }
+        bool TryPeek(int count, out T data);
+        bool Read(int count);
+    }
+
     public interface IInput<Y>
     {
         Y Current { get; }
