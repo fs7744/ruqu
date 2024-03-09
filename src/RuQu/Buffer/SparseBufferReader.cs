74169726255303e3d392c88faab44eefc6008a64
victor.x.qu <FS7744@HOTMAIL.COM>
2024-03-09 23:41:46 +0800

    add sparse buffer test begin

--- src/RuQu/Buffer/SparseBufferReader.cs
+++ src/RuQu/Buffer/SparseBufferReader.cs
@@ -1,44 +1,138 @@
-﻿using System;
-using System.Buffers;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection.PortableExecutable;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Buffers;
 
 namespace RuQu.Buffer
 {
-    public interface IBuffer<T> : IDisposable
+    public interface IChunk<T>: IDisposable
     {
-        public int ConsumedCount { get; }
-        public int Index { get; }
+        public ReadOnlySpan<T> Span { get; }
+        public ReadOnlyMemory<T> Memory { get; }
 
+        public IChunk<char>? Next();
         public void Consume(int count);
-
     }
-
 
     public class TextSparseBufferReader
     {
-
         private readonly unsafe delegate*<int, ref int, int> growth;
         private readonly TextReader reader;
-        private readonly ArrayPool<char> pool;
+        internal readonly ArrayPool<char> pool;
+        private int chunkSize;
+        private int chunkIndex;
+        private IChunk<char> currentChunk;
 
-        public TextSparseBufferReader(TextReader reader, ArrayPool<char> pool, int chunkSize = 256, SparseBufferGrowth growth = SparseBufferGrowth.None)
+        public TextSparseBufferReader(TextReader reader, ArrayPool<char> pool, int chunkSize = 128, SparseBufferGrowth growth = SparseBufferGrowth.None)
         {
             ArgumentOutOfRangeException.ThrowIfNegativeOrZero(chunkSize);
             this.reader = reader;
             this.pool = pool;
+            this.chunkSize = chunkSize;
             unsafe
             {
                 this.growth = growth.GetFunc();
-            }    
+            }
         }
 
-        public TextSparseBufferReader(TextReader reader, int chunkSize = 256, SparseBufferGrowth growth = SparseBufferGrowth.None) : this(reader, ArrayPool<char>.Shared, chunkSize, growth)
+        public TextSparseBufferReader(TextReader reader, int chunkSize = 128, SparseBufferGrowth growth = SparseBufferGrowth.None) : this(reader, ArrayPool<char>.Shared, chunkSize, growth)
         {
+        }
+
+        internal class Chunk : ReadOnlySequenceSegment<char>, IChunk<char>
+        {
+            private char[] array;
+            internal int index;
+            private readonly TextSparseBufferReader textSparseBufferReader;
+
+            new ReadOnlyMemory<char> Memory => base.Memory.Slice(index);
+            ReadOnlySpan<char> IChunk<char>.Span => base.Memory.Span.Slice(index);
+
+            public Chunk(char[] array, int count, TextSparseBufferReader textSparseBufferReader)
+            {
+                base.Memory = array.AsMemory(0, count);
+                this.array = array;
+                this.textSparseBufferReader = textSparseBufferReader;
+            }
+
+            public void Dispose()
+            {
+                if (array.Length != 0)
+                {
+                    textSparseBufferReader.pool.Return(array);
+                }
+            }
+
+            public IChunk<char>? Next()
+            {
+                if (base.Memory.IsEmpty) return null;
+                if (base.Next == null)
+                {
+                    var r = textSparseBufferReader.NextBuffer();
+                    base.Next = r;
+                }
+                return base.Next as IChunk<char>;
+            }
+
+            public void Consume(int count)
+            {
+                if (index >= base.Memory.Length) return;
+                index += count;
+                if (index >= base.Memory.Length)
+                {
+                    if (base.Next is Chunk chunk)
+                    {
+                        textSparseBufferReader.currentChunk = chunk;
+                        this.Dispose();
+                        chunk.Consume(index - Memory.Length);
+                    }
+                    index = base.Memory.Length;
+                }
+            }
+        }
+
+        public unsafe IChunk<char> GetCurrentChunk()
+        {
+            if (currentChunk == null)
+            {
+                currentChunk = NextBuffer();
+                if (currentChunk == null)
+                {
+                    currentChunk = new Chunk(Array.Empty<char>(), 0, this);
+                }
+            }
+            return currentChunk;
+        }
+
+        internal unsafe Chunk? NextBuffer()
+        {
+            var buffer = pool.Rent(growth(chunkSize, ref chunkIndex));
+            try
+            {
+                var count = 0;
+                do
+                {
+                    int readCount = reader.Read(buffer.AsSpan(count));
+                    if (readCount == 0)
+                    {
+                        break;
+                    }
+
+                    count += readCount;
+                }
+                while (count < buffer.Length);
+                if (count == 0)
+                {
+                    pool.Return(buffer);
+                }
+                else
+                {
+                    return new Chunk(buffer, count, this);
+                }
+            }
+            catch
+            {
+                pool.Return(buffer);
+                throw;
+            }
+            return null;
         }
     }
 }
